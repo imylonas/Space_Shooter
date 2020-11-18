61218c2048325c7c91d939b8781d7dfbc596f96c
imylonas <ioannis.k.mylonas@gmail.com>
2020-11-18 21:19:32 +0200

    Collusion

--- Assets/Game/Scripts/PowerUp.cs
+++ Assets/Game/Scripts/PowerUp.cs
@@ -6,16 +6,16 @@ public class PowerUp : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+    }
+
 }
